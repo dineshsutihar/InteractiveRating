769dff4a24cfe98be7b8578faeafd0f0ad7c2e09
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-11-17 16:18:07 +0530

    Add link to script.js file and update button and li hover styles

---
+++ script.js
@@ -0,0 +1,25 @@
+var numbers=document.querySelectorAll("li");
+var OutputRating=document.querySelector("spam");
+var buttom=document.querySelector("buttom");
+var content1=document.querySelector("#container1")
+var content2=document.querySelector("#container2")
+
+
+
+numbers.forEach((number, index) => {
+    number.addEventListener('click', () => {
+      resetNumbers();
+      setActiveNumbers(index + 1);
+      OutputRating.textContent = index + 1;
+    });
+});
+
+
+
+screen.addEventListener("click",function(){
+    content1.style.display="none";
+    // removing the display none from container 2
+    content2.style.display="block";
+
+
+})
