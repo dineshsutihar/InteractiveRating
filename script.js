f65ddc84314697509028f6c4c44168215d05fa52
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-11-17 22:41:31 +0530

    Fix rating display and add click functionality to rating stars.

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-var numbers=document.querySelectorAll("li");
-var OutputRating=document.querySelector("spam");
-var buttom=document.querySelector("buttom");
-var content1=document.querySelector("#container1")
-var content2=document.querySelector("#container2")
+document.addEventListener('DOMContentLoaded', function () {
+    var numbers = document.querySelectorAll("li");
+    var OutputRating = document.querySelector("#outputRating");
+    var button = document.querySelector("button");
+    var content1 = document.querySelector("#container1");
+    var content2 = document.querySelector("#container2");
 
 
 
-numbers.forEach((number, index) => {
-    number.addEventListener('click', () => {
-      resetNumbers();
-      setActiveNumbers(index + 1);
-      OutputRating.textContent = index + 1;
+    console.log(numbers);
+    console.log(OutputRating);
+    console.log(button);
+  
+    numbers.forEach((li, index) => {
+      li.addEventListener('click', () => {
+        resetNumbers();
+        OutputRating.textContent = index + 1;
+        li.classList.add('clicked');
+      });
     });
-});
+    function resetNumbers() {
+        numbers.forEach(li => li.classList.remove('clicked'));
+      }
 
 
-
-buttom.addEventListener("click",function(){
-    content1.style.display="none";
-    // removing the display none from container 2
-    content2.style.display="block";
-
-
-})
+  
+    button.addEventListener("click", function () {
+      content1.style.display = "none";
+      // removing the display none from container 2
+      content2.style.display = "block";
+    });
+  });
+  
